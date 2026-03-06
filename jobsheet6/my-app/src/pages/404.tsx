4c8df7e6a0c3aad6874f79ca8e29496a27c15517
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-03-06 08:12:15 +0700

    tugas 3

--- jobsheet6/my-app/src/pages/404.tsx
+++ jobsheet6/my-app/src/pages/404.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/404.module.scss';
 
 const Custom404 = () => {
@@ -10,6 +11,9 @@ const Custom404 = () => {
             <img src="/404.svg" alt="404" className={styles.error__image} />
             <h1 className={styles.error__title}>404 - Halaman tidak ditemukan</h1>
             <p className={styles.error__desc}>Maaf, halaman yang Anda cari tidak ada.</p>
+            <Link href="/" className={styles.error__button}>
+                Kembali ke Beranda
+            </Link>
         </div>
     );
 };
