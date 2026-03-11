d94b874024b1b69b1d949740a6ee282d11cf99bd
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-03-11 11:03:50 +0700

    Langkah 3: Fetch Data API di Frontend

--- jobsheet7/my-app/src/pages/produk/index.tsx
+++ jobsheet7/my-app/src/pages/produk/index.tsx
@@ -1,11 +1,62 @@
-import ProdukView from '../views/produk';
+// import ProdukView from '../views/produk';
 
-const produk = () => {
+// const produk = () => {
+//     return (
+//         <>
+//             <ProdukView />
+//         </>
+//     );
+// };
+
+// export default produk;
+
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+type ProductType = {
+    id: string;
+    nama: string;
+    harga: number;
+    ukuran: string;
+    warna: string;
+}
+
+const kategori = () => {
+    // const [isLogin, setIsLogin] = useState(false);
+    // const { push } = useRouter();
+    const [products, setProducts] = useState([]);
+
+    // useEffect(() => {
+    //     if (!isLogin) {
+    //         push('/login');
+    //     }
+    // }, []);
+
+    useEffect(() => {
+        fetch("api/produk")
+            .then((response) => response.json())
+            .then((responsedata) => {
+                // console.log("Data Produk:", responsedata.data);
+                setProducts(responsedata.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching produk:", error);
+            });
+    }, []);
+
     return (
-        <>
-            <ProdukView />
-        </>
+        <div>
+            <h1>Daftar Produk</h1>
+            {products.map((product: ProductType) => (
+                <div key={product.id}>
+                    <h2>{product.nama}</h2>
+                    <p>Harga: {product.harga}</p>
+                    <p>Ukuran: {product.ukuran}</p>
+                    <p>Warna: {product.warna}</p>
+                </div>
+            ))}
+        </div>
     );
 };
 
-export default produk;
+export default kategori;    
