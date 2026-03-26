fc087e761b8aaeb65fca203248f7145c9bf847c7
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-03-26 17:17:14 +0700

    Lagkah 3: Implementasi Skeleton Loading

--- jobsheet8/my-app/src/pages/views/produk/index.tsx
+++ jobsheet8/my-app/src/pages/views/produk/index.tsx
@@ -13,15 +13,27 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
         <div className={styles.produk}>
             <h1 className={styles.produk__title}>Daftar Produk</h1>
             <div className={styles.produk__content}>
-            {products.map((product: ProductType) => (
-                <div key={product.id} className={styles.produk__content__item}>
-                    <img src={product.image} alt={product.name} className={styles.produk__content__item__image} width="200" />
-                    <h4 className={styles.produk__content__item__name}>{product.name}</h4>
-                    <p className={styles.produk__content__item__price}>Harga: Rp {product.price.toLocaleString()}</p>
-                    <p className={styles.produk__content__item__size}>Ukuran: {product.size}</p>
-                    <p className={styles.produk__content__item__category}>Kategori: {product.category}</p>
+                {products.length > 0 ? (
+                    <>
+                    {products.map((product: ProductType) => (
+                        <div key={product.id} className={styles.produk__content__item}>
+                            <img src={product.image} alt={product.name} className={styles.produk__content__item__image} width="200" />
+                            <h4 className={styles.produk__content__item__name}>{product.name}</h4>
+                            <p className={styles.produk__content__item__price}>Harga: Rp {product.price.toLocaleString()}</p>
+                            <p className={styles.produk__content__item__size}>Ukuran: {product.size}</p>
+                            <p className={styles.produk__content__item__category}>Kategori: {product.category}</p>
+                        </div>
+                    ))}
+                    </>
+            ) : (
+                <div className={styles.produk__content__skeleton}>
+                    <div className={styles.produk__content__skeleton__image}></div>
+                    <div className={styles.produk__content__skeleton__name}></div>
+                    <div className={styles.produk__content__skeleton__price}></div>
+                    <div className={styles.produk__content__skeleton__size}></div>
+                    <div className={styles.produk__content__skeleton__category}></div>
                 </div>
-            ))}
+            )}
             </div>
         </div>
     );
