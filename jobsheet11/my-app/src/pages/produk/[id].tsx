240d1e93a7266032741142d9387e7edf42fa675f
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-03-31 19:12:26 +0700

    Langkah 4: Implementasi Static Site Generation (Dynamic SSG)

--- jobsheet11/my-app/src/pages/produk/[id].tsx
+++ jobsheet11/my-app/src/pages/produk/[id].tsx
@@ -30,17 +30,53 @@ export default HalamanProduk;
 
 // {digunakan server side rendering (SSR) untuk mengambil data produk berdasarkan ID dari URL}
 
-export async function getServerSideProps({ params }: { params: { id: string } }) {
-  const res = await fetch(`http://localhost:3000/api/produk/${params.id}`);
+// export async function getServerSideProps({ params }: { params: { id: string } }) {
+//   const res = await fetch(`http://localhost:3000/api/produk/${params.id}`);
+//   const respone = await res.json();
+
+//   if (!respone.data) {
+//     return { notFound: true };
+//   }
+
+//   return {
+//     props: {
+//       product: respone.data,
+//     },
+//   };
+// }
+
+
+
+// Digunakan  static site generation (SSG) untuk mengambil data produk berdasarkan ID dari URL.
+export async function getStaticPaths() {
+  const res = await fetch('http://localhost:3000/api/produk');
   const respone = await res.json();
 
-  if (!respone.data) {
-    return { notFound: true };
+  const paths = respone.data.map((product: ProductType) => ({
+    params: { id: product.id }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  };
+  // console.log("Paths yang dihasilkan getStaticPaths:", paths); // Debugging: tampilkan paths yang dihasilkan
+  return {
+    paths,
+    fallback: false
   }
+}
+
+export async function getStaticProps({ params }: { params: { id: string } }) {
+  const res = await fetch(`http://localhost:3000/api/produk/${params.id}`);
+  // const response: ProductType = await res.json();
+  const response: { data: ProductType } = await res.json();
+
+  // console.log("Data produk yang diambil dari API:", response); // Debugging: tampilkan data produk yang diambil
 
   return {
     props: {
-      product: respone.data,
-    },
-  };
+      product: response.data,
+    }
+  }
 }
