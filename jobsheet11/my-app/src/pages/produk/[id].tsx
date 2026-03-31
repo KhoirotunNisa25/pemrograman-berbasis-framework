625d97b3d31f398e1e7ee3ac44d271a9cf018094
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-03-31 18:52:43 +0700

    Langkah 3: Implementasi SSR

--- jobsheet11/my-app/src/pages/produk/[id].tsx
+++ jobsheet11/my-app/src/pages/produk/[id].tsx
@@ -2,15 +2,45 @@ import fetcher from "../../util/swr/fetcher";
 import {useRouter} from 'next/router';
 import useSWR from 'swr';
 import DetailProduk from '../../views/DetailProduk';
+import produk from '../../../../../jobsheet05/my-app/src/pages/produk/index';
+import { ProductType } from "@/types/Product.type";
 
-const HalamanProduk = () => {
-   const {query} = useRouter();
-    const {data, error, isLoading} = useSWR(`/api/produk/${query.id}`, fetcher);
-    return (
-        <div>
-          <DetailProduk products={isLoading ? [] : data.data}></DetailProduk>
-        </div>
-    );
+const HalamanProduk = ({product}:{product: ProductType}) => {
+  // Digunakan client side rendering (CSR) untuk mengambil data produk berdasarkan ID dari URL
+  //  const {query} = useRouter();
+  //   const {data, error, isLoading} = useSWR(`/api/produk/${query.id}`, fetcher);
+  //   return (
+  //       <div>
+  //         <DetailProduk products={isLoading ? [] : data.data}></DetailProduk>
+  //       </div>
+  //   );
+
+  return (
+    <div>
+      <DetailProduk products={product} />
+    </div>
+  )
 };
 
 export default HalamanProduk;
+
+// Fungsi getServerSideProps untuk mengambil data produk berdasarkan ID dari URL secara server side rendering (SSR).
+// akan dipanggil setiap kali halaman diakses, sehingga data produk selalu terbaru.
+// mengambil data produk dari API menggunakan fetcher dan mengembalikan data produk sebagai props ke komponen HalamanProduk.
+
+// {digunakan server side rendering (SSR) untuk mengambil data produk berdasarkan ID dari URL}
+
+export async function getServerSideProps({ params }: { params: { id: string } }) {
+  const res = await fetch(`http://localhost:3000/api/produk/${params.id}`);
+  const respone = await res.json();
+
+  if (!respone.data) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      product: respone.data,
+    },
+  };
+}
