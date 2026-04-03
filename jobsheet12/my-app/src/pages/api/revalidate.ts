f922b3109382ac29c86f2a0c5f9513159bb495aa
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-04-03 19:19:06 +0700

    Langkah 2 (D): Tambahkan Parameter Data

--- jobsheet12/my-app/src/pages/api/revalidate.ts
+++ jobsheet12/my-app/src/pages/api/revalidate.ts
@@ -4,22 +4,27 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
     revalidated: boolean;
+    message?: string;
 };
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    try  {
-        await res.revalidate("/produk/static");
-        return res.status(200).json({ revalidated: true });
-    } catch (error) {
-        console.error("Error in API route:", error);
-        res.status(500).send({ revalidated: false });
+    if (req.query.data === "produk") {
+        try  {
+            await res.revalidate("/produk/static");
+            return res.status(200).json({ revalidated: true });
+        } catch (error) {
+            console.error("Error in API route:", error);
+            res.status(500).send({ revalidated: false });
+        }
     }
+
+    return res.json({
+        revalidated: false,
+        message: "Invalid query parameter. Expected 'data=produk'. Please provide 'data=produk' to trigger revalidation."
+    });
 }
 
-// Note : jika kita menggunakan code ini ada sedikit masalah yaitu Ketika salah satu data dihapus
-// maka data yang terhapus tidak secara otomatis terupdate pada layar meskipun sudah di
-// refresh selama 10 detik. Untuk bisa terupdate data yang ditampilkan diperlukan trigger
-// dengan mengakses apinya http://localhost:3000/api/produk
+
