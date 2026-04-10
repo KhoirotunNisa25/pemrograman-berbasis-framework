0d627f432223160d5df54bad28a8fb223d8060fd
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-04-10 19:59:04 +0700

    C. Langkah 4: Batasi Route Tertentu

--- jobsheet13/my-app/src/middleware.ts
+++ jobsheet13/my-app/src/middleware.ts
@@ -2,8 +2,12 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    console.log("middleware");
+    // console.log("middleware");
     return NextResponse.redirect(new URL("/", request.url));
 
     // return NextResponse.next();
 }
+
+export const config = {
+    matcher: ["/produk", "/about"],
+};
