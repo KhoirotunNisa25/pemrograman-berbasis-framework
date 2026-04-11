3da9786077d605b83d37e2c7d500b2d222e5e267
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-04-11 10:42:21 +0700

    C. Langkah 4: Tambahkan SessionProvider

--- jobsheet14/my-app/src/pages/_app.tsx
+++ jobsheet14/my-app/src/pages/_app.tsx
@@ -2,11 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '@/components/layout/navbar'
 import AppShell from '@/components/layout/AppShell'
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps : { session, ...pageProps} }: AppProps) {
   return (
+    <SessionProvider session={pageProps.session}>
       <AppShell>
         <Component {...pageProps} />
       </AppShell>
+    </SessionProvider>
   );
 };
