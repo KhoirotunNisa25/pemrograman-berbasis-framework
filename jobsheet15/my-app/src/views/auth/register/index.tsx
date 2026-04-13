2f7312a585a2f21f2c5deab635d5aaaf80ec516e
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-04-13 21:31:06 +0700

    C. Langkah 1: Membuat Register View

--- jobsheet15/my-app/src/views/auth/register/index.tsx
+++ jobsheet15/my-app/src/views/auth/register/index.tsx
@@ -4,17 +4,77 @@ import { useRouter } from "next/router";
 import styles from "./register.module.css";
 
 const TampilanRegister = () => {
-    const { push } = useRouter();
-    const handlerRegister = () => {
-        console.log("register berhasil");
-        push('/auth/login');
-    }
+    // const { push } = useRouter();
+    // const handlerRegister = () => {
+    //     console.log("register berhasil");
+    //     push('/auth/login');
+    // }
     return (
-        <div className={styles.login}>
-            <h1 className="text-3xl font-bold text-blue-600">Halaman Register</h1>
-            <button onClick={() => handlerRegister()}>Register</button> <br/>
-            <h1 style={{ color: "red",border: "1px solid red",borderRadius: "5px",padding: "5px",}}>sudah punya akun</h1>
-            <Link href="/auth/login">Ke Halaman Login</Link>
+        <div className={styles.register}>
+            <h1 className={styles.register__title}>Halaman Register</h1>
+            {/* <button onClick={() => handlerRegister()}>Register</button> <br/> */}
+            {/* <h1 style={{ color: "red",border: "1px solid red",borderRadius: "5px",padding: "5px",}}>sudah punya akun</h1> */}
+
+            <div className={styles.register__form}>
+                <form action="">
+                    <div className={styles.register__form__item}>
+                        <label
+                            htmlFor="email"
+                            className={styles.register__form__item__label}
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            className={styles.register__form__item__input}
+                        />
+                    </div>
+
+                    <div className={styles.register__form__item}>
+                        <label
+                            htmlFor="Fullname"
+                            className={styles.register__form__item__label}
+                        >
+                            Fullname
+                        </label>
+                        <input
+                            type="text"
+                            id="fullname"
+                            name="fullname"
+                            placeholder="Fullname"
+                            className={styles.register__form__item__input}
+                        />
+                    </div>
+
+                    <div className={styles.register__form__item}>
+                        <label
+                            htmlFor="password"
+                            className={styles.register__form__item__label}
+                        >
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            className={styles.register__form__item__input}
+                        />
+                    </div>
+
+                    <button type="submit" className={styles.register__form__item__button}>
+                        Register
+                    </button>
+                </form>
+
+                <br />
+                <p className={styles.register__form__item__text}>
+                    Sudah punya akun? <Link href="/auth/login">Ke Halaman Login</Link>
+                </p>
+            </div>
         </div>
     );
 };
