d3f9f85e528ec4792150e3f3be9ff908c6b001f5
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-04-20 22:15:43 +0700

    C. Langkah 6: Membuat halaman Admin dan authoriz

--- jobsheet16/my-app/src/Middleware/withAuth.ts
+++ jobsheet16/my-app/src/Middleware/withAuth.ts
@@ -1,6 +1,8 @@
 import { getToken } from "next-auth/jwt";
 import { NextMiddleware, NextRequest, NextFetchEvent, NextResponse } from "next/server";
 
+const hanyaAdmin = ["/admin"];
+
 export default function withAuth(
   middleware: NextMiddleware,
   requireAuth: string[] = []
@@ -13,14 +15,15 @@ export default function withAuth(
         req,
         secret: process.env.NEXTAUTH_SECRET,
       });
-
       if (!token) {
         const Url = new URL("/auth/login", req.url);
         Url.searchParams.set("callbackUrl", encodeURI(req.url));
         return NextResponse.redirect(Url);
       }
+      if (token.role !=="admin" && hanyaAdmin.includes(pathname)) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
     }
-    
     return middleware(req, next);
   };
 }
---
+++ jobsheet16/my-app/src/pages/admin/index.tsx
@@ -0,0 +1,16 @@
+const HalamanAdmin = () => {
+    return (
+        <div>
+            <div className="admin">
+                <h1>Halaman Admin</h1>
+                <p>
+                    Selamat datang di halaman admin! Di sini Anda dapat mengelola konten, pengguna, dan pengaturan lainnya untuk aplikasi Anda. 
+                    Pastikan untuk menjaga keamanan informasi dan hanya memberikan akses kepada orang yang tepat. 
+                    Terima kasih telah menggunakan aplikasi kami!
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default HalamanAdmin;
