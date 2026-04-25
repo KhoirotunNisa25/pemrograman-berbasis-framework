4e6acc12caefba3477e28e6899d2257de04bedff
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-04-25 21:03:49 +0700

    Tugas Praktikum 3: Tambahkan script Google Analytics menggunakan next/script

--- jobsheet18/my-app/src/pages/_app.tsx
+++ jobsheet18/my-app/src/pages/_app.tsx
@@ -3,13 +3,37 @@ import type { AppProps } from 'next/app'
 import Navbar from '@/components/layout/navbar'
 import AppShell from '@/components/layout/AppShell'
 import { SessionProvider } from "next-auth/react";
+import Script from 'next/script';
 
 export default function App({ Component, pageProps : { session, ...pageProps} }: AppProps) {
+  const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
   return (
-    <SessionProvider session={pageProps.session}>
-      <AppShell>
-        <Component {...pageProps} />
-      </AppShell>
-    </SessionProvider>
+    <>
+      <SessionProvider session={session}>
+        <AppShell>
+          <Component {...pageProps} />
+        </AppShell>
+      </SessionProvider>
+      
+      {/* Google Analytics */}
+      {GA_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', {
+                page_path: window.location.pathname,
+              });
+            `}
+          </Script>
+        </>
+      )}
+    </>
   );
 };
