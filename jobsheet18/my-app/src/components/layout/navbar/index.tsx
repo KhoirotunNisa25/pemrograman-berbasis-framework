2ff16fd700980b7a81016feb46b713890bddc731
KhoirotunNisa25 <khoirotunnisa2507@gmail.com>
2026-04-25 20:40:36 +0700

    Praktikum 4: Optimasi Avatar dengan next/image

--- jobsheet18/my-app/src/components/layout/navbar/index.tsx
+++ jobsheet18/my-app/src/components/layout/navbar/index.tsx
@@ -26,8 +26,8 @@ const Navbar = () => {
                 <Image 
                   src={data.user.image} 
                   alt={data.user.fullname} 
-                  width={40}
-                  height={40}
+                  width={50}
+                  height={50}
                   className={styles.navbar__user__image}
                 />
               )}
